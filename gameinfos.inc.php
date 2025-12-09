b5e0a29e83c5fc523425e1fdd6cf07db39f3b7d7
Joni Ahola <joni.ahola@agendadigital.fi>
2025-12-10 00:20:05 +0200

    Update gameinfos.inc.php

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -11,22 +11,22 @@
 
 $gameinfos = [
     // Name of the game in English (will serve as the basis for translation)
-    'game_name' => "My Great Game",
+    'game_name' => "Linko!",
 
     // Game publisher (use empty string if there is no publisher)
-    'publisher' => 'My Publishing Company',
+    'publisher' => 'AMIGO',
 
     // Url of game publisher website
     'publisher_website' => 'http://www.mypublishingcompany.com/',
 
     // Board Game Geek ID of the publisher
-    'publisher_bgg_id' => 1234,
+    'publisher_bgg_id' => 8,
 
     // Board game geek ID of the game
-    'bgg_id' => 0,
+    'bgg_id' => 153065,
 
     // Players configuration that can be played (ex: 2 to 4 players)
-    'players' => [2, 3, 4],
+    'players' => [2, 3, 4, 5],
 
     // Suggest players to play with this number of players. Must be null if there is no such advice, or if there is only one possible player configuration.
     // NB: the automatic lobby will try first the lowest number of players if this is not specified. So you _have to_ specify this parameter if the lowest player number is not compatible with the default options.
@@ -37,7 +37,7 @@ $gameinfos = [
     // 'not_recommend_player_number' => array( 2, 3 ),      // <= example: this is not recommended to play this game with 2 or 3 players
 
     // Estimated game duration, in minutes (used only for the launch, afterward the real duration is computed)
-    'estimated_duration' => 30,
+    'estimated_duration' => 20,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = fast)
     'fast_additional_time' => 30,
